161886dbe782b83290bd91823e207f8ae98eb585
siwon9 <siwon10823@gmail.com>
2024-06-01 16:48:05 +0900

    Controller에 Runnable 인터페이스 상속, Startable 인터페이스 정의수정

--- Project/src/main/java/org/choongang/global/Controller.java
+++ Project/src/main/java/org/choongang/global/Controller.java
@@ -1,6 +1,6 @@
 package org.choongang.global;
 
-public interface Controller extendsgig Runnable{
+public interface Controller extends Runnable{
     void show(); // 서비스(Model)와 뷰를 연결
 
     default void setMenu(Menu menu){
