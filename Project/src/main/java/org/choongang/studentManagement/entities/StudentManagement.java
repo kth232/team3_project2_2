56f1043f81f9525fd6115be578b8daeed3be10a5
soeunl <soeun318@gmail.com>
2024-06-03 16:25:53 +0900

    StudentManagementMapper 쿼리문 작성

--- Project/src/main/java/org/choongang/studentManagement/entities/StudentManagement.java
+++ Project/src/main/java/org/choongang/studentManagement/entities/StudentManagement.java
@@ -1,4 +1,16 @@
 package org.choongang.studentManagement.entities;
 
+import lombok.Builder;
+import lombok.Data;
+
+@Builder
+@Data
 public class StudentManagement {
+        private String studentName;
+        private String studentClass;
+        private String studentSubject;
+        private String studentAddress;
+        private String studentPhone;
 }
+
+
